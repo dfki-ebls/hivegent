2e94787b188eb64c3a0cb7405f380e5b4d777511
Mirko Lenz <mirko@mirkolenz.com>
2026-04-27 13:51:29 +0200

    build(backend): update derivation

--- backend/default.nix
+++ backend/default.nix
@@ -8,21 +8,67 @@
 }:
 let
   workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
-  pyprojectOverlay = workspace.mkPyprojectOverlay {
+  projectOverlay = workspace.mkPyprojectOverlay {
     sourcePreference = "wheel";
   };
+  getCudaPkgs = attrs: lib.filter (name: lib.hasPrefix "nvidia-" name) (lib.attrNames attrs);
+  cudaOverlay =
+    final: prev:
+    lib.genAttrs (getCudaPkgs prev) (
+      name:
+      prev.${name}.overrideAttrs (old: {
+        autoPatchelfIgnoreMissingDeps = true;
+      })
+    );
+  buildSystemOverlay =
+    final: prev:
+    lib.mapAttrs
+      (
+        name: value:
+        prev.${name}.overrideAttrs (old: {
+          nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ (final.resolveBuildSystem value);
+        })
+      )
+      {
+        antlr4-python3-runtime.setuptools = [ ];
+        ebooklib.setuptools = [ ];
+        pylatexenc.setuptools = [ ];
+      };
+  packageOverlay =
+    final: prev:
+    lib.mapAttrs (name: value: prev.${name}.overrideAttrs value) {
+      torch = old: {
+        autoPatchelfIgnoreMissingDeps = true;
+      };
+      torchvision = old: {
+        autoPatchelfIgnoreMissingDeps = true;
+      };
+      kreuzberg = old: {
+        autoPatchelfIgnoreMissingDeps = true;
+      };
+    };
   baseSet = callPackage pyproject-nix.build.packages {
     python = python3;
   };
   pythonSet = baseSet.overrideScope (
     lib.composeManyExtensions [
       pyproject-build-systems.overlays.wheel
-      pyprojectOverlay
+      projectOverlay
+      cudaOverlay
+      buildSystemOverlay
+      packageOverlay
     ]
   );
+  mkVenv =
+    name: deps:
+    (pythonSet.mkVirtualEnv name deps).overrideAttrs (_: {
+      venvIgnoreCollisions = [
+        "${python3.sitePackages}/cv2/*"
+      ];
+    });
   inherit (callPackage pyproject-nix.build.util { }) mkApplication;
 in
 mkApplication {
-  venv = pythonSet.mkVirtualEnv "hivegent-env" workspace.deps.optionals;
+  venv = mkVenv "hivegent-env" workspace.deps.optionals;
   package = pythonSet.hivegent;
 }
