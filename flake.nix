6fcf574f080d0c169ca9ba149c5d0ce39dd67f97
Mirko Lenz <mirko@mirkolenz.com>
2026-07-14 10:28:37 +0200

    chore(deps/nix): update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
     systems.url = "github:nix-systems/default";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
